3464acb6d01ff0053d369a3e79954aae5d78023a
MaveriK198 <provastonage@gmail.com>
2020-07-29 18:17:17 +0200

    fix span computation

--- Scripts/text_generation/BertRanker.py
+++ Scripts/text_generation/BertRanker.py
@@ -140,7 +140,7 @@ class BertRanker():
 
                 ranked_list = self.rank_concept(sentence,
                                                     sentence_context,
-                                                    Span(pre_context_len+match.start()-1,pre_context_len+match.end()-1),
+                                                    Span(pre_context_len+match.start()-1,pre_context_len+match.end()),
                                                     concept.alternatives)
 
                 ret[concept.concept] = ranked_list
